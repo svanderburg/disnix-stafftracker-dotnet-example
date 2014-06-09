5951b47a2ed8f97afaf2bb5bb1adbaed501c045b
Sander van der Burg <svanderburg@gmail.com>
2014-06-09 16:51:40 +0200

    Adopt new Hydra convention

--- release.nix
+++ release.nix
@@ -1,24 +1,22 @@
 { nixpkgs ? <nixpkgs>
-, nixos ? <nixos>
+, disnix_stafftracker_dotnet_example ? {outPath = ./.; rev = 1234;}
+, officialRelease ? false
 }:
 
 let
   jobs = rec {
     tarball =
-      { disnix_stafftracker_dotnet_example ? {outPath = ./.; rev = 1234;}
-      , officialRelease ? false}:
-    
       let
         pkgs = import nixpkgs {};
         
         disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
-          inherit nixpkgs nixos;
+          inherit nixpkgs;
         };
       in
       disnixos.sourceTarball {
         name = "disnix_stafftracker_dotnet_example";
         version = builtins.readFile ./version;
-        src = WebServicesExampleNET;
+        src = disnix_stafftracker_dotnet_example;
         inherit officialRelease;
       };
   };
