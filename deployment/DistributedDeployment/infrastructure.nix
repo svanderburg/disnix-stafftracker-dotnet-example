ee643a78fcab244f77c31dcb9be793c18f6d7808
Sander van der Burg <svanderburg@gmail.com>
2016-05-06 23:06:42 +0000

    Convert infrastructure model to new convention

--- deployment/DistributedDeployment/infrastructure.nix
+++ deployment/DistributedDeployment/infrastructure.nix
@@ -1,8 +1,18 @@
 {
   test1 = {
-    hostname = "localhost";
-    msSqlUsername = "sa";
-    msSqlPassword = builtins.readFile ./mssqlpw;
-    iisWwwRoot = "/cygdrive/c/inetpub/wwwroot";
+    properties = {
+      hostname = "localhost";
+    };
+    
+    containers = {
+      mssql-database = {
+        msSqlUsername = "sa";
+        msSqlPassword = builtins.readFile ./mssqlpw;
+      };
+      
+      iis-webapplication = {
+        iisWwwRoot = "/cygdrive/c/inetpub/wwwroot";
+      };
+    };
   };
 }
